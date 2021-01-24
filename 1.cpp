66e54b07fe9a91a4fcad2a529833078257c28fc0
Avanish Gupta <ui19co15@iiitsurat.ac.in>
2021-01-24 11:54:02 +0530

    Fixed indentation to problem

--- 1.cpp
+++ 1.cpp
@@ -1,15 +1,19 @@
-class Solution {
+class Solution
+{
 public:
-  vector<int> twoSum(vector<int>& nums, int target) {
-    unordered_map<int, int> map;
-    map.reserve(nums.size());
-    for(int i = 0; i < nums.size(); ++i) {
-      const auto it = map.find(target - nums[i]);
-      if(it != end(map)) {
-        return { i, it->second };
-      }
-      map.emplace(nums[i], i);
+    vector<int> twoSum(vector<int> &nums, int target)
+    {
+        unordered_map<int, int> map;
+        map.reserve(nums.size());
+        for (int i = 0; i < nums.size(); ++i)
+        {
+            const auto it = map.find(target - nums[i]);
+            if (it != end(map))
+            {
+                return {i, it->second};
+            }
+            map.emplace(nums[i], i);
+        }
+        throw invalid_argument("Input has no solution");
     }
-    throw invalid_argument("Input has no solution");
-  }
 };
